710ea54431e687424b89f2b229aea518b36dcce8
Jul1en-Lin <bigtigerlinw@gmail.com>
2026-07-23 16:07:20 +0800

    md mstemplate

---
+++ lien-mstemplate/lien-mstemplate-api/src/main/java/com/lien/mstemplateapi/MstemplateApiApplication.java
@@ -0,0 +1,12 @@
+package com.lien.mstemplateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstemplateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MstemplateApiApplication.class, args);
+    }
+}
---
+++ lien-mstemplate/lien-mstemplate-service/src/main/java/com/lien/mstemplateservice/MstemplateServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lien.mstemplateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstemplateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MstemplateServiceApplication.class, args);
+    }
+}
